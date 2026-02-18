52bc502c9f7a266ddbf54c0dee9f3da1c7050510
gyennifer751-create <gyennifer751@gmail.com>
2026-02-17 21:00:50 -0500

    Create MacCheckbox.java

---
+++ src/proudcts/MacCheckbox.java
@@ -0,0 +1,10 @@
+package src.products;
+import src.interfaces.Checkbox;
+
+public class MacCheckbox implements Checkbox {
+    @Override
+    public void render() {
+        // TODO: Implementar renderizado estilo Mac (ej. check azul)
+        System.out.println("Renderizando un checkbox estilo MacOS.");
+    }
+}
