656033b20c2ba2b026c84de87cb074007ae54e72
gyennifer751-create <gyennifer751@gmail.com>
2026-02-17 20:59:37 -0500

    Create WinCheckbox.java

---
+++ src/proudcts/WinCheckbox.java
@@ -0,0 +1,10 @@
+package src.products;
+import src.interfaces.Checkbox;
+
+public class WinCheckbox implements Checkbox {
+    @Override
+    public void render() {
+        // TODO: Implementar renderizado estilo Windows (ej. check con 'X')
+        System.out.println("Renderizando un checkbox estilo Windows.");
+    }
+}
